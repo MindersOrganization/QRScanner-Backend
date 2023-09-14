a18622721207e365b7ec2d3c4979811169d6ead0
M-AshrafDawood <ashraf.dawood2295@gmail.com>
2023-09-14 20:54:41 +0300

    .

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -6,7 +6,7 @@ set -e
 host="qr_db"
 cmd="$@"
 
-until mysql -h "$host" -P 3307 -u "minders_qr_admin" --password="Minders@2023" -e 'SELECT 1'; do
+until mysql -h "$host" -P 3306 -u "minders_qr_admin" --password="Minders@2023" -e 'SELECT 1'; do
   >&2 echo "MySQL is unavailable - sleeping"
   sleep 1
 done
