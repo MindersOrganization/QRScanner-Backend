6d4ddfe0eb8495601d58d6bcccea7a1e4bcb696d
M-AshrafDawood <ashraf.dawood2295@gmail.com>
2023-09-16 08:47:47 +0300

    Added ticket design with static images and added time_block function to allow mails to be sent only from 8 am to just before 11 pm, also adjusted the hourly limit to 33 mails per hour

--- QRScanner/management/commands/send_tickets_mail.py
+++ QRScanner/management/commands/send_tickets_mail.py
@@ -1,14 +1,99 @@
-import time
 from email.mime.image import MIMEImage
-
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.core.mail import get_connection
 
 from QRScanner.models import Attendee
-
 from django.core.management.base import BaseCommand
 
+import time
+from datetime import datetime, timedelta
+
+
+def attach_qr_code(msg, person):
+    # Open the image file in binary mode
+    with open(person.qr_code.path, 'rb') as img:
+        # Create a MIMEImage
+        msg_img = MIMEImage(img.read(), _subtype='png')
+        # Define the Content-ID header
+        msg_img.add_header('Content-ID', '<qr_code>')
+        # Attach the image
+        msg.attach(msg_img)
+    return msg
+
+
+# Global variable to store images
+cached_images = {}
+
+
+def cache_images():
+    static_images = [
+        {
+            'path': 'media/ADMIT-ONE.png',
+            'extension': 'png',
+            'cid': 'admit_one'
+        },
+        {
+            'path': 'media/First-Step-Logo_.png',
+            'extension': 'png',
+            'cid': 'first_step_logo'
+        },
+        {
+            'path': 'media/Main-Ticket.png',
+            'extension': 'png',
+            'cid': 'main_ticket'
+        },
+        {
+            'path': 'media/Minders-Logo.png',
+            'extension': 'png',
+            'cid': 'minders_logo'
+        },
+        {
+            'path': 'media/Scan-Me.png',
+            'extension': 'png',
+            'cid': 'scan_me'
+        }
+    ]
+    for image in static_images:
+        with open(image['path'], 'rb') as img:
+            # Cache the image data
+            cached_images[image['cid']] = img.read()
+
+    print("Static have been cached")
+
+
+def attach_static_media(msg):
+    for image in cached_images:
+        # Create a MIMEImage from the cached data
+        msg_img = MIMEImage(cached_images[image], _subtype='png')
+        # Define the Content-ID header
+        msg_img.add_header('Content-ID', image)
+        # Attach the image
+        msg.attach(msg_img)
+    return msg
+
+
+def attach_media(msg, person):
+    msg = attach_qr_code(msg, person)
+    msg = attach_static_media(msg)
+    return msg
+
+
+def time_block():
+    current_time = datetime.now()
+    if current_time.hour < 8 or current_time.hour >= 23:
+        print("Sleeping until 8 AM.")
+        tomorrow = datetime.now() + timedelta(1)
+        tomorrow_at_8 = datetime(year=tomorrow.year, month=tomorrow.month, day=tomorrow.day, hour=8)
+        time.sleep((tomorrow_at_8 - datetime.now()).seconds)
+
+
+def sleep_until_next_hour():
+    current_time = datetime.now()
+    next_hour = (current_time + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
+    sleep_time = (next_hour - current_time).total_seconds()
+    print(f"Sleeping for {sleep_time} seconds until {next_hour}.")
+    time.sleep(sleep_time)
 
 def send_ticket_email(persons):
     subject = "Minders'23 First Step - Style Your Future"
@@ -23,33 +108,29 @@ def send_ticket_email(persons):
             msg = EmailMultiAlternatives(subject, '', from_email, [to])
             msg.attach_alternative(html_content, "text/html")
 
-            # Open the image file in binary mode
-            with open(person.qr_code.path, 'rb') as img:
-                # Create a MIMEImage
-                msg_img = MIMEImage(img.read(), _subtype='png')
-                # Define the Content-ID header
-                msg_img.add_header('Content-ID', '<qr_code>')
-                # Attach the image
-                msg.attach(msg_img)
+            msg = attach_media(msg, person)
+
+            time_block()
 
             msg.send()
             person.has_received_email = True
             person.save()
-            print("({}/{}) -> Sent mail to {}.".format(counter, size, person.full_name))
+            print("{}/{} Sent mail to {} at {}".format(counter, size, person.full_name, person.email))
             counter += 1
 
-            if (counter - 1) % 20 == 0:
-                print("Mails are sent, sleeping for an hour")
-                time.sleep(3600)
+            if (counter - 1) % 33 == 0:
+                print("Sent 20 mails, sleeping for an hour")
+                sleep_until_next_hour()
 
 
 class Command(BaseCommand):
     help = "Send mails to all users who didn't receive ticket mails yet"
 
     def handle(self, *args, **options):
+        cache_images()
         try:
             while Attendee.objects.filter(has_received_email=False).exists():
-                send_ticket_email(Attendee.objects.filter(has_received_email=False))
+                send_ticket_email(list(Attendee.objects.filter(has_received_email=False)))
         except AttributeError:
             pass
         finally:
